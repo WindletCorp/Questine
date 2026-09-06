11f7a7983baf1b96503d6675dc6bd6b31dc274e6
Ayden <aydenyellsow@gmail.com>
2026-09-06 17:38:01 +0530

    fix: hydration errors.

--- themes/default/pages/AppHome.tsx
+++ themes/default/pages/AppHome.tsx
@@ -17,11 +17,6 @@ import { EntryView } from "../components/entry-view";
 export function AppHome({ userId }: { userId?: string }) {
   const [entryModeActive, setEntryModeActive] = useState(false);
   const [timelineItems, setTimelineItems] = useState<TimelineItem[]>([]);
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   useEffect(() => {
     async function loadData() {
@@ -45,12 +40,6 @@ export function AppHome({ userId }: { userId?: string }) {
   const nextRoutine = timelineItems.find(i => i.type === "routine_block" && new Date((i.data as RoutineBlock).end_time).getTime() > Date.now())?.data as RoutineBlock | undefined;
   const latestMetric = timelineItems.slice().reverse().find(i => i.type === "metric_entry")?.data as EnrichedMetricEntry | undefined;
   const latestJournal = timelineItems.slice().reverse().find(i => i.type === "journal")?.data as Journal | undefined;
-
-  if (!mounted) {
-    return (
-      <div className="relative flex flex-col justify-between p-5 md:p-6 antialiased select-none flex-1 h-full w-full max-w-lg mx-auto opacity-0" />
-    );
-  }
 
   return (
     <div className="relative flex flex-col justify-between p-5 md:p-6 antialiased select-none flex-1 h-full w-full max-w-lg mx-auto">
